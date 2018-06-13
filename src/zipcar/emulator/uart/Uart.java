b49abeae126e604a60754122afc36c06c1a9bdeb
Neil Goldader <cgoldader@zipcar.com>
2018-06-13 15:40:53 -0700

    implemented rudimentary read capabilities with an SFRObserver, may refactor down the line

--- src/zipcar/emulator/uart/Uart.java
+++ src/zipcar/emulator/uart/Uart.java
@@ -8,22 +8,25 @@ import com.microchip.mplab.mdbcore.simulator.scl.SCL;
 import com.microchip.mplab.mdbcore.simulator.MessageHandler;
 import com.microchip.mplab.mdbcore.simulator.SimulatorDataStore.SimulatorDataStore;
 import com.microchip.mplab.mdbcore.simulator.PeripheralSet;
+import com.microchip.mplab.mdbcore.simulator.SFR.SFRObserver;
 import java.util.LinkedList;
 import org.openide.util.lookup.ServiceProvider;
 
 @ServiceProvider(path=Peripheral.REGISTRATION_PATH, service=Peripheral.class)
-public class uart implements Peripheral {
+public class Uart implements Peripheral {
 
     static MessageHandler messageHandler = null;
     static SFR sfrBuff = null;
     static SFR sfrInterrupt = null;    
     static SFR sfrSTA = null;
+    static SFR sfrTX = null;
     int updateCounter = 0;
     static SFRSet sfrs;
     SCL scl;
     boolean notInitialized = true;
     int cycleCount = 0;
     LinkedList<Character> chars = new LinkedList();
+    MySFRObserver sfrObs = new MySFRObserver();
     
     @Override
     public boolean init(SimulatorDataStore DS) {
@@ -33,6 +36,7 @@ public class uart implements Peripheral {
         sfrBuff = sfrs.getSFR("U2RXREG");
         sfrInterrupt = sfrs.getSFR("IFS1");
         sfrSTA = sfrs.getSFR("U2STA");
+        sfrTX = sfrs.getSFR("U2TXREG");
         
         // remove UART2
         PeripheralSet periphSet = DS.getPeripheralSet();
@@ -75,9 +79,10 @@ public class uart implements Peripheral {
     @Override
     public void update() {
         if (cycleCount % 1000000 == 0) {
+            sfrTX.addObserver(sfrObs);
             if (!chars.isEmpty()) {
                 if (sfrSTA.getFieldValue("UTXEN") == 1) {
-                    messageHandler.outputMessage(chars.peek() + "");
+                    // messageHandler.outputMessage(chars.peek() + "");
                     sfrBuff.privilegedWrite(chars.pop());   // Inject the next char
                     sfrInterrupt.privilegedSetFieldValue("U2RXIF", 1); // Trigger the interrupt
                     // sfrSTA.privilegedSetFieldValue("URXDA", 1); // Trigger the STA
@@ -92,6 +97,20 @@ public class uart implements Peripheral {
     public void setString(String str) {
         for (int i = 0; i < str.length(); i++){
             chars.add(str.charAt(i));
+        }
+    }
+    
+    public static void output() {
+        messageHandler.outputMessage((char) sfrTX.read() + "");
+    }
+}
+
+class MySFRObserver implements SFRObserver {
+    
+    @Override
+    public void update(SFR generator, SFREvent event, SFREventSource source) {
+        if (event == SFREvent.SFR_CHANGED) {
+            Uart.output();
         }
     }
 }
