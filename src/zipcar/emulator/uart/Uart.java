b294853dc989b14b0a0cf15ce4987fdc216d3b4d
Neil Goldader <cgoldader@zipcar.com>
2018-06-18 10:00:19 -0700

    Cleaned up code, added comments

--- src/zipcar/emulator/uart/Uart.java
+++ src/zipcar/emulator/uart/Uart.java
@@ -17,34 +17,39 @@ import org.openide.util.lookup.ServiceProvider;
 @ServiceProvider(path = Peripheral.REGISTRATION_PATH, service = Peripheral.class)
 public class Uart implements Peripheral {
 
+    final String UART_NUM = "UART2"; // UART Name (eg: UART1, UART2, etc...)
+    final String UART_RX = "U2RXREG"; // Respective UART RX SFR
+    final String UART_INTERRUPT = "IFS1"; // Respective UART Interrupt SFR
+    final String UART_STA = "U2STA"; // Respective UART STA SFR
+    final String UART_TX = "U2TXREG"; // Respective UART TX SFR
+    
+    static Uart instance;
     MessageHandler messageHandler;
-    SFR sfrBuff;
+    SFR sfrRX;
     SFR sfrInterrupt;
     SFR sfrSTA;
     SFR sfrTX;
-    int updateCounter = 0;
     SFRSet sfrs;
+    int updateCounter = 0;
+    int cycleCount = 0;
     SCL scl;
     boolean notInitialized = true;
-    int cycleCount = 0;
     LinkedList<Character> chars = new LinkedList<Character>();
     FileOutputStream request;
     FileInputStream response;
-    static Uart instance;
-    // BufferedOutputStream request;
-    // BufferedInputStream response;
     
     @Override
     public boolean init(SimulatorDataStore DS) {
-        // initialize instance variables
+        
+        // Initialize instance variables
         messageHandler = DS.getMessageHandler();
         sfrs = DS.getSFRSet();
-        sfrBuff = sfrs.getSFR("U2RXREG");
-        sfrInterrupt = sfrs.getSFR("IFS1");
-        sfrSTA = sfrs.getSFR("U2STA");
-        sfrTX = sfrs.getSFR("U2TXREG");
+        sfrRX = sfrs.getSFR(UART_RX);
+        sfrInterrupt = sfrs.getSFR(UART_INTERRUPT);
+        sfrSTA = sfrs.getSFR(UART_STA);
+        sfrTX = sfrs.getSFR(UART_TX);
         
-        // remove UART2
+        // Remove default UART2
         PeripheralSet periphSet = DS.getPeripheralSet();
         Peripheral uartPeriph = periphSet.getPeripheral("UART2");
         if (uartPeriph != null) {
@@ -52,26 +57,24 @@ public class Uart implements Peripheral {
             periphSet.removePeripheral(uartPeriph);
         }
 
-        // setup pipes
+        // Setup pipes
         try {
             request = new FileOutputStream("/Users/cgoldader/pic-brain/LMBrain.X/sim/req");
             response = new FileInputStream("/Users/cgoldader/pic-brain/LMBrain.X/sim/res");
-            // request = new BufferedOutputStream(reqStream);
-            // response = new BufferedInputStream(resStream);
             
         } catch (FileNotFoundException e) {
-            messageHandler.outputMessage("Exception in update: " + e);
+            messageHandler.outputMessage("Exception in init: " + e);
             return false;
         }
         
-        // add observers
+        // Add observers
         UartObserver obs = new UartObserver();
         sfrTX.addObserver(obs);
         
         messageHandler.outputMessage("External Peripheral Initialized: UART");
-        
         instance = this;
-        // add peripheral to list and return true
+        
+        // Add peripheral to list and return true
         DS.getPeripheralSet().addToActivePeripheralList(this);
         return true;
     }
@@ -88,7 +91,7 @@ public class Uart implements Peripheral {
 
     @Override
     public String getName() {
-        return "UART2_SIM";
+        return UART_NUM + "_SIM";
     }
 
     @Override
@@ -105,18 +108,18 @@ public class Uart implements Peripheral {
     public void update() {
         if (cycleCount % 267 == 0) {
             try {
-                if (response.available() != 0) {
-                    messageHandler.outputMessage("available bytes: " + response.available());
-                    chars.add((char) response.read()); // Doesn't currently work!!!!!!!
+                if (response.available() != 0) { // If there are unread bytes, read them and add the chars
+                    messageHandler.outputMessage("Available bytes: " + response.available());
+                    chars.add((char) response.read());
                 }
             } catch (IOException e) {
                 messageHandler.outputMessage("Exception reading character from res " + e);
                 return;
             }
-            if (!chars.isEmpty()) {
-                if (sfrSTA.getFieldValue("UTXEN") == 1) {
-                    // messageHandler.outputMessage(chars.peek() + "");
-                    sfrBuff.privilegedWrite(chars.pop());   // Inject the next char
+            if (!chars.isEmpty()) { // Inject anything in chars
+                if (sfrSTA.getFieldValue("UTXEN") == 1) { // If STA is ready to receive !! IMPORTANT
+                    // messageHandler.outputMessage(chars.peek() + ""); // Returns the next char which will be injected
+                    sfrRX.privilegedWrite(chars.pop()); // Inject the next char
                     sfrInterrupt.privilegedSetFieldValue("U2RXIF", 1); // Trigger the interrupt
                 }
             }
@@ -124,20 +127,20 @@ public class Uart implements Peripheral {
         cycleCount++;
     }
 
+    // Debugging function for manually adding a string to chars
     public void setString(String str) {
         for (int i = 0; i < str.length(); i++) {
             chars.add(str.charAt(i));
         }
     }
-
+    
+    // Try to write bytes to the request file
     public void output() {
         try {
-            // messageHandler.outputMessage("Should be outputting " + sfrTX.read());
             request.write((byte) sfrTX.read()); 
         } catch (Exception e) {
             messageHandler.outputMessage("failed to write request byte " + e);
         }
-        // messageHandler.outputMessage((char) sfrTX.read() + "");
     }
     
     public static Uart get() {
